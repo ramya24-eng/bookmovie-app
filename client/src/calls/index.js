580d9b617a822fcbe71969ebc5a3be0242a3a633
Ramya <60138815+ramya24-eng@users.noreply.github.com>
2024-11-07 17:08:22 +0530

    Update index.js

--- client/src/calls/index.js
+++ client/src/calls/index.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 
 export const axiosInstance = axios.create({
+    baseURL: process.env.REACT_APP_API_URL,
     headers : {
-
         'Content-Type' : 'application/json',
         'authorization' : `Bearer ${localStorage.getItem('token')}`
     }
